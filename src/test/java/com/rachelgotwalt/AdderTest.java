e34bc876667b1a8dc83ac8c88f7a017b26dac4c5
Rachel Gotwalt <rachel.gotwalt@berwickinsurance.com>
2019-07-17 14:22:22 -0700

    Fixed test name

--- src/test/java/com/rachelgotwalt/AdderTest.java
+++ src/test/java/com/rachelgotwalt/AdderTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class AdderTest {
 
     @Test
-    public void add_should_add_two_numbers() {
+    public void add_should_add_two_positive_numbers() {
        assertEquals(2, Adder.add(1, 1));
     }
 
